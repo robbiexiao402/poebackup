4afd9932fcc317dc0302611263730dfb39eead7f
Yufei Xiao <ST10490551@vcconnect.edu.za>
2025-04-17 13:29:53 +0200

    Update ChatAppCS.java

--- ChatAppCS.java
+++ ChatAppCS.java
@@ -3,8 +3,10 @@ package com.mycompany.chatappcs;
 import java.util.Scanner;
 
 public class ChatAppCS {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter username: ");
         String username = scanner.nextLine();
         System.out.println("Enter password: ");

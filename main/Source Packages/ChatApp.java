5d3ad681e5ad199da2bdebe84340cd0ecfc5724f
Yufei Xiao <ST10490551@vcconnect.edu.za>
2025-04-22 13:45:43 +0200

    Create ChatApp.java

---
+++ main/Source Packages/ChatApp.java
@@ -0,0 +1,35 @@
+package com.mycompany.chatappcs;
+
+import java.util.Scanner;
+
+public class ChatAppCS {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.println("Enter password: ");
+        String password = scanner.nextLine();
+        System.out.println("Enter number: ");
+        String number = scanner.nextLine();
+
+        Login newLogin = new Login(username, password, number);
+
+        if (newLogin.checkUsername() && newLogin.checkPassword() && newLogin.checkCellPhoneNumber()) {
+            System.out.println(newLogin.registerUser());
+            
+            System.out.println("Confirm username: ");
+            String confirmUsername = scanner.nextLine();
+            System.out.println("Confirm password: ");
+            String confirmPassword = scanner.nextLine();
+            System.out.println("Confirm number: ");
+            String confirmNumber = scanner.nextLine();
+            
+            boolean status = newLogin.loginUser(confirmUsername, confirmPassword, confirmNumber);
+            System.out.println(newLogin.LoginStatus(status));
+        } else {
+            System.out.println(newLogin.registerUser());
+        }
+    }
+}
